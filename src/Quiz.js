81cbb51a922adbb73ccad75439cc6facc235b145
Eric Schwartz <erics273@gmail.com>
2021-05-13 19:34:31 -0400

    module5

--- src/Quiz.js
+++ src/Quiz.js
@@ -4,6 +4,7 @@ import QuizEnd from "./QuizEnd";
 
 let quizData = require('./quiz_data.json');
 
+
 class Quiz extends Component {
 
     constructor(props){
@@ -11,11 +12,15 @@ class Quiz extends Component {
         this.state = {quiz_position: 1};
     }
 
+    showNextQuestion(){
+        this.setState({quiz_position: this.state.quiz_position +1})
+    }
+
     render(){
         const isQuizEnd = (this.state.quiz_position -1) === quizData.quiz_questions.length;
         return (
             <div>
-                {isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}  />}
+                {isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} />}
             </div>
         );
     }
